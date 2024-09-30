1e95608db35c26ca735b64fb1c3210c180428308
Vinh Nguyen <cathng11@gmail.com>
2024-09-30 15:58:17 +1000

    set up concurrently to run all services with labeled logs

--- backend/packages/api-gateway/src/main.ts
+++ backend/packages/api-gateway/src/main.ts
@@ -1,21 +1,21 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
 import {
   ExpressAdapter,
   NestExpressApplication,
-} from '@nestjs/platform-express';
-import * as morgan from 'morgan';
-import { AllExceptionsFilter } from './all-exceptions.filter';
-import * as express from 'express';
-import * as cookieParser from 'cookie-parser';
-import { json, urlencoded } from 'express';
+} from "@nestjs/platform-express";
+import * as morgan from "morgan";
+import { AllExceptionsFilter } from "./all-exceptions.filter";
+import * as express from "express";
+import * as cookieParser from "cookie-parser";
+import { json, urlencoded } from "express";
 
 const PORT = 7810;
 
 const server: express.Express = express();
 async function createNestServer(serverExpress: express.Express) {
-  const logger: any[] = ['error', 'warn', 'debug'].concat(
-    process.env.ENV != 'production' ? ['log'] : [],
+  const logger: any[] = ["error", "warn", "debug"].concat(
+    process.env.ENV != "production" ? ["log"] : []
   );
   const adapter = new ExpressAdapter(serverExpress);
   const app = await NestFactory.create<NestExpressApplication>(
@@ -24,7 +24,7 @@ async function createNestServer(serverExpress: express.Express) {
     {
       cors: true,
       logger,
-    },
+    }
   );
 
   app.use(
@@ -32,18 +32,18 @@ async function createNestServer(serverExpress: express.Express) {
       ':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :response-time ms :res[content-length] ":referrer" ":user-agent" ',
       {
         skip: function (req) {
-          return req.originalUrl.startsWith('/health') || req.method == 'GET';
+          return req.url.startsWith("/health") || req.method == "GET";
         },
-      },
-    ),
+      }
+    )
   );
-  app.use(json({ limit: '1mb' }));
-  app.use(urlencoded({ extended: true, limit: '1mb' }));
+  app.use(json({ limit: "1mb" }));
+  app.use(urlencoded({ extended: true, limit: "1mb" }));
   app.use(cookieParser());
   app.useGlobalFilters(new AllExceptionsFilter());
 
   // Health check path
-  app.use('/health', (req, res) => {
+  app.use("/health", (req, res) => {
     res.sendStatus(200);
   });
 
